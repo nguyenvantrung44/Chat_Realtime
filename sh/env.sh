efe35f3806fd01dfd583c5637cb53f59f85a55eb
nguyenvantrung44 <66216602+nguyenvantrung44@users.noreply.github.com>
2021-11-07 21:06:06 +0700

    Update env.sh

--- sh/env.sh
+++ sh/env.sh
@@ -7,8 +7,8 @@ export DB_USERNAME=""
 export DB_PASSWORD=""
 
 #config app enviroment
-export APP_HOST=localhost
-export APP_PORT=8017
+export APP_HOST=ec2-54-169-181-35.ap-southeast-1.compute.amazonaws.com
+export APP_PORT=3000
 
 #config session key and secret
 export SESSION_KEY="express.sid"
@@ -23,9 +23,9 @@ export MAIL_PORT=587
 #config facebook login app
 export FB_APP_ID=392070039003221
 export FB_APP_SECRET=acbae07db6c2a673981e41191d3a8213
-export FB_CALLBACK_URL=https://localhost:8017/auth/facebook/callback
+export FB_CALLBACK_URL=http://ec2-54-169-181-35.ap-southeast-1.compute.amazonaws.com:3000/auth/facebook/callback
 
 #config google login app
 export GG_APP_ID=489064763664-78pgraqhi8geqedbgu82cd2s1af5m9l0.apps.googleusercontent.com
 export GG_APP_SECRET=-9KXcsLGLkjm77aFJn3sQ7Bk
-export GG_CALLBACK_URL=https://localhost:8017/auth/google/callback
+export GG_CALLBACK_URL=http://ec2-54-169-181-35.ap-southeast-1.compute.amazonaws.com:3000/auth/google/callback
